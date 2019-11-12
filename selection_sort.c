e5f9362aae80e11d7b1001fde1a8fe173ba715e5
Aluno <eduardo_acj2@hotmail.com>
2019-11-11 23:41:56 -0200

    comentarios

--- selection_sort.c
+++ selection_sort.c
@@ -4,6 +4,8 @@
 #include <sys/time.h>
 #include <omp.h>
 
+//struct para consumir a linha de cache por inteiro.
+//tecnica de padding
 typedef struct line
 {
 	int n;
@@ -12,23 +14,42 @@ typedef struct line
 
 void selection_sort (int *vetor,int max)
 {
+	//iniciando variaveis
   int i, j, m, aux, div, mod, id, nt;
+	
+	/*
+		Struct que serve para aplicar a tecnica de padding
+		e utilizar a linha inteira de uma cache de 64bits. 
+		Pode ser substituida por um int normal.
+	*/
 	Line *min = NULL;
 
+	//For para percorrer o vetor inteiro
   for (i = 0; i < (max - 1); i++) {
 
+		//Iniciando contexto paralelo
 		#pragma omp parallel private (id, j)
 		{
 
+			//Obtendo identificador da thread atual
 			id = omp_get_thread_num();
 
+			/*
+				Disparando instrução para ser utilizada na primeira thread.
+				Esta instrução será executada apenas uma vez.
+			*/
 			#pragma omp single
 			{
+				//Obtendo o numero de threads disponíveis
 				nt = omp_get_num_threads();
+				//Caso o valor min ainda não esteja inicializado. Inicia-se com o calloc
 				if(min == NULL)
+					//aloca-se memória para um vetor de nt posições
 					min = calloc(nt, sizeof(Line));
+
 			}
 
+			//Atribuindo o valor na posição referente a thread atual
 			min[id].n = i;
 
 			/*
@@ -44,7 +65,7 @@ void selection_sort (int *vetor,int max)
 		}
 
 		m = 0;
-
+		
 		for(j = 1; j < nt; j++){
 			if(vetor[min[j].n] < vetor[min[m].n])
 				m = j;
